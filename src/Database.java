87e63ef62ae566df543796c1e963d7c294e23176
Xskullibur <simasas2222@gmail.com>
2017-11-27 15:18:16 +0800

    [Done] - Add Desktop - Add Laptop - Remove Desktop - Remove Laptop - Display All Info - Added Back Database - Quit
    [To Be Added]
    + Unique ID
    + Display Desktop / Laptop Only
    + Edit Array Info

---
+++ src/Database.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Database extends Computer
+{
+    private ArrayList<Desktop> desktop = new ArrayList<>();
+    private ArrayList<Laptop> laptop = new ArrayList<>();
+
+    public void addDesktop(Desktop d)
+    {
+        desktop.add(d);
+    }
+
+    public void addLaptop (Laptop l)
+    {
+        laptop.add(l);
+    }
+
+    public void removeDesktop(int Di)
+    {
+        desktop.remove(Di);
+    }
+
+    public void removeLaptop(int Li)
+    {
+        laptop.remove(Li);
+    }
+
+    public int DesktopIndexOf(Desktop DI)
+    {
+        return desktop.indexOf(DI);
+    }
+
+    public int LaptopIndexOf(Laptop LI)
+    {
+        return laptop.indexOf(LI);
+    }
+
+    public int DesktopSize() {return desktop.size();}
+
+    public int LaptopSize() {return laptop.size();}
+
+    public ArrayList<Desktop> getDesktop() { return desktop; }
+
+    public ArrayList<Laptop> getLaptop() { return laptop; }
+}
